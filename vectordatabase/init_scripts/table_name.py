f2e848e0829a48a86ae538509caef83e8b61436e
tbjuechen <1324374092@qq.com>
2025-03-02 02:54:04 +0800

    feat: table name knowledge base

--- vectordatabase/init_scripts/table_name.py
+++ vectordatabase/init_scripts/table_name.py
@@ -3,4 +3,22 @@ import sys
 
 # 获取当前脚本所在目录的上级目录
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-from vdb import client
+from vdb import client, Item, embedding
+
+collection = client.get_or_create_collection('table_name')
+
+datas = [
+    Item(ids='概览数据（日）', documents='summary_data_day'),
+    Item(ids='概览数据日表', documents='summary_data_day'),
+    Item(ids='概览数据(月)', documents='summary_data_month'),
+    Item(ids='概览数据月表', documents='summary_data_month'),
+]
+
+for data in datas:
+    collection.add(**data.to_dict())
+
+# try
+query = '概览数据(月)'
+query_embedding = embedding(query)
+result = collection.query(query_embedding)
+print(result)
--- vectordatabase/vdb.py
+++ vectordatabase/vdb.py
@@ -3,9 +3,24 @@ vector database api
 '''
 import chromadb
 from pathlib import Path
+from typing import Callable
+
+from embedding import embedding
 
 
 db_file = 'chromadb.db'
 
 client = chromadb.PersistentClient(path=db_file)
 
+class Item:
+    def __init__(self,ids:str ,documents:str ,embedding:Callable=embedding):
+        self.ids = ids
+        self.documents = documents
+        self.embedding = embedding(ids)
+
+    def to_dict(self):
+        return {
+            'ids': self.ids,
+            'documents': self.documents,
+            'embeddings': self.embedding
+        }
