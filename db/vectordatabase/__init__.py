66fe8bae638c4c1bfa18058cddbfbcfadad031cf
tbjuechen <1324374092@qq.com>
2025-04-25 00:03:24 +0800

    feat: document knowledge base

--- db/vectordatabase/__init__.py
+++ db/vectordatabase/__init__.py
@@ -1,4 +1,5 @@
 from .vdb import client, Item, BaseCollection
+from utils.data import text_split
 
 create_collection_class = lambda name, elements: type(f"{name}Collection", (BaseCollection,), {
     "__init__": lambda self, client: BaseCollection.__init__(self, name, client),
@@ -10,5 +11,15 @@ table_name_collection:BaseCollection = create_collection_class(
     {
         "add": lambda self, ids, document: self._add(Item(ids, document, metadata={"type": "table_name", "table": document})),
         "get_all": lambda self, table_name: self.get(where={"table": table_name})
+    }
+)(client)
+
+document_collection:BaseCollection = create_collection_class(
+    "document",
+    {
+        "add": lambda self, title, document: map(
+            lambda x: self._add(Item(title + x[0], x[1], metadata={"type": "document", "title": title, "index": x[0]})), 
+            enumerate(text_split(document))
+        ),
     }
 )(client)
--- utils/data.py
+++ utils/data.py
@@ -1,4 +1,5 @@
 import numpy as np
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 def dicts_to_markdown_table(data):
     if not data:
@@ -27,3 +28,20 @@ def convert_array_to_list(data):
         return data.tolist()
     else:
         return data  # 否则，返回原始值
+    
+def text_split(content: str, chunk_size: int = 500, chunk_overlap: int = 50) -> list:
+    """
+    将文本分割成多个块
+    :param content: 要分割的文本
+    :param chunk_size: 每个块的大小
+    :param chunk_overlap: 块之间的重叠大小
+    :return: 分割后的文本块列表
+    """
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        separators=["\n\n", "\n", "。", ".", "！", "？", ",", "，", " "]
+    )
+    docs = text_splitter.create_documents([content])
+    chunks = [doc.page_content for doc in docs]
+    return chunks
